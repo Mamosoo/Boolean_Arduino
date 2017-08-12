778772f075b7b0ecb3cc37958efd60d0dba6a244
왕대성 <bentforwork@gmail.com>
2017-08-12 22:24:55 +0900

    K-ICT Hackathon Study

---
+++ boolexample/boolexample.ino
@@ -0,0 +1,37 @@
+int click_pin = 2;
+int click_press = LOW;
+bool check = true;
+int click_count = 0;
+
+void setup() {
+
+  Serial.begin(9600);
+
+}
+
+void loop() {
+
+  pinMode(click_pin, INPUT);
+  click_press = digitalRead(click_pin);
+
+  if (click_press == HIGH)
+  {
+    if (check)
+    {
+      check = false;
+      Serial.print(++click_count);
+      Serial.print("\n");
+
+    }
+
+  }
+  else
+  {
+    check = true;
+  }
+
+  delay(50);
+
+
+
+}
